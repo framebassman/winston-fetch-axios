b8005773126996adab56622c1a45774dab363008
Aerilym <aerilymbiz@gmail.com>
2022-08-19 02:08:34 +1000

    Update dependencies to specify winston-transport

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { createLogger, transports, format } from 'winston';
 import Transport from 'winston-transport';
 import axios from 'axios';
 
@@ -13,10 +12,6 @@ interface AxiosTransportOptions extends Transport.TransportStreamOptions {
   replacer?: (key: string, value: any) => any;
 }
 
-//
-// Inherit from `winston-transport` so you can take advantage
-// of the base functionality and `.exceptions.handle()`.
-//
 export class AxiosTransport extends Transport {
   host: string;
   auth: string;
@@ -24,13 +19,6 @@ export class AxiosTransport extends Transport {
 
   constructor(opts: AxiosTransportOptions) {
     super(opts);
-
-    //
-    // Consume any custom options here. e.g.:
-    // - Connection information for databases
-    // - Authentication information for APIs (e.g. loggly, papertrail,
-    //   logentries, etc.).
-    //
     this.host = opts.host;
     this.auth = opts.auth;
     this.path = opts.path;
@@ -51,8 +39,6 @@ export class AxiosTransport extends Transport {
       .catch(function (error) {
         return error;
       });
-
-    // Perform the writing to the remote service
 
     callback();
   }
