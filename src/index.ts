7a41617256ddd6f37a03101d90af8be3ffe1d99d
Aerilym <aerilymbiz@gmail.com>
2022-08-20 14:42:28 +1000

    Major overhaul of transport

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,60 @@
 import Transport from 'winston-transport';
-import axios from 'axios';
+import axios, { AxiosRequestConfig, AxiosRequestHeaders } from 'axios';
 
+type TransportAuthType = 'bearer' | 'apikey' | 'basic' | 'custom' | 'none';
+type TransportMethod = 'POST' | 'PUT';
+
+/**
+ * Options for Axios Transport.
+ * @param {string} url - The url to send the logs to.
+ * @param {string} path - The path to send the logs to. The destination url will resolve to url + path.
+ * @param {string} auth - The authentication token to send with the logs. Will override any auth headers provided in {@link headers}.
+ * @param {TransportAuthType} authType - The type of authentication to use.
+ * @param {TransportMethod} method - The method to use when sending the logs.
+ * @param {AxiosRequestHeaders} headers - The headers to send with the logs.
+ */
 interface AxiosTransportOptions extends Transport.TransportStreamOptions {
-  host: string;
-  auth: string;
-  path: string;
-  headers?: object;
-  batch?: boolean;
-  batchInterval?: number;
-  batchCount?: number;
+  url?: string;
+  path?: string;
+  auth?: string;
+  authType?: TransportAuthType;
+  method?: TransportMethod;
+  headers?: AxiosRequestHeaders;
+  host?: string; // @deprecated use url instead
   replacer?: (key: string, value: any) => any;
 }
 
+/**
+ * Transport for Winston that sends log messages to a remote server using Axios.
+ * @param {AxiosTransportOptions} options - The options for the transport.
+ * @see {@link AxiosTransportOptions}
+ * @example
+ * const logger = createLogger({
+ *   transports: [
+ *     new AxiosTransport({
+ *       host: 'http://localhost:3000',
+ *       path: '/logs'
+ *     }),
+ *   ],
+ * });
+ * logger.log({ level: 'info', message: 'Hello World' });
+ */
 export class AxiosTransport extends Transport {
-  host: string;
-  auth: string;
-  path: string;
+  url: string;
+  path?: string;
+  auth?: string;
+  authType?: TransportAuthType;
+  method?: TransportMethod;
+  headers?: AxiosRequestHeaders;
 
   constructor(opts: AxiosTransportOptions) {
     super(opts);
-    this.host = opts.host;
-    this.auth = opts.auth;
-    this.path = opts.path;
+    this.url = opts.url || opts.host || 'http://localhost:80';
+    this.path = opts.path || undefined;
+    this.auth = opts.auth || undefined;
+    this.authType = opts.authType || undefined;
+    this.method = opts.method || 'POST';
+    this.headers = opts.headers || undefined;
   }
 
   log(info: any, callback: () => void) {
@@ -29,10 +62,65 @@ export class AxiosTransport extends Transport {
       this.emit('logged', info);
     });
 
-    axios
-      .post(this.host + this.path, info, {
-        headers: { Authorization: 'Bearer ' + this.auth },
-      })
+    // Resolve the destination url.
+    let resolvedUrl = this.url;
+    if (this.path) {
+      let resolvedPath = this.path;
+      if (resolvedUrl.endsWith('/')) {
+        resolvedUrl = resolvedUrl.slice(0, -1);
+      }
+      if (!resolvedPath.startsWith('/')) {
+        resolvedPath = '/' + resolvedPath;
+      }
+      resolvedUrl = resolvedUrl + resolvedPath;
+    }
+
+    let axiosConfig: AxiosRequestConfig<any> = {
+      method: this.method,
+      url: resolvedUrl,
+      data: info,
+    };
+
+    // Validate and format headers
+    if (this.headers) {
+      // If auth is provided but also defined in headers, remove it from headers
+      if (this.headers.authorization && this.auth) {
+        delete this.headers.authorization;
+      }
+
+      // If headers are defined, add them to the axios config
+      if (Object.keys(this.headers).length > 0) {
+        axiosConfig = { ...axiosConfig, headers: this.headers };
+      }
+    }
+
+    // If auth is defined, add it to the axios config
+    if (this.auth) {
+      const authType = this.authType || 'bearer';
+      let authPrefix;
+      switch (authType) {
+        case 'bearer':
+          authPrefix = 'Bearer ';
+          break;
+        case 'apikey':
+          authPrefix = 'ApiKey ';
+          break;
+        case 'basic':
+          authPrefix = 'Basic ';
+          break;
+        default:
+          authPrefix = '';
+          break;
+      }
+      const authHeader = `${authPrefix}${this.auth}`;
+      if (axiosConfig.headers) {
+        axiosConfig.headers = { ...axiosConfig.headers, authorization: authHeader };
+      } else {
+        axiosConfig.headers = { authorization: authHeader };
+      }
+    }
+
+    axios(axiosConfig)
       .then(function (response) {
         return response;
       })
